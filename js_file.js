4eb8d4d7688284d4533c91984886497fb7a1ccaa
Andres Shankara Rovalo <116384505+Shankaraa2001@users.noreply.github.com>
2023-10-30 00:10:14 -0700

    Delete js_file.js

--- js_file.js
+++
@@ -1,8 +0,0 @@
-var icon = document.querySelector('.icon');
-var topnav = document.getElementById('myTopnav');
-
-function toggleMenu() {
-    topnav.classList.toggle('responsive');
-}
-
-icon.addEventListener('click', toggleMenu);
